ce68c546378608f1ece6404a81e5feab1282edcd
codingwith-sahil <sahilsaifi2607@gmail.com>
2026-03-02 17:47:53 +0530

    Add files via upload
    Day 01 - Created a simple calculator program performing +, -, *, / using user input

---
+++ arithmatic operation.py
@@ -0,0 +1,10 @@
+a=int(input("enter the first number: "))
+b=int(input("enter the second number: "))
+sum=a+b
+print("the sum of two number is : ",sum)
+diff=a-b
+print("the difference of two number is : ",diff)
+multi=a*b
+print("the multiplication of two number is : ",multi)
+div=a/b
+print("the division of two number is : ",div)
