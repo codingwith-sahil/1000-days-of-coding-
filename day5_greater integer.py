5ffadcd1e7d6233f1b4c76b46c9906af81d98048
sahil <sahilsaifi2607@gmail.com>
2026-03-07 18:11:25 +0530

    Add files via upload
    on day 05 of my thousand days coding challenge i made two projects 
    1 to find greatest integer between two numbers
    2 to find whether the number is integer or not

---
+++ day5_greater integer.py
@@ -0,0 +1,8 @@
+#a program to find greatest integer among two numbers 
+num_1=int(input("enter the first number : "))
+num_2=int(input("enter the second number : "))
+if num_1>num_2:
+    print("first number is greater ")
+else:
+    print("second number is greater ")
+    
---
+++ day_5 even or odd.py
@@ -0,0 +1,6 @@
+#prgram to check number is odd or even
+num=int(input("enter the first number : "))
+if num%2==0:
+    print("the number is an even number ")
+else:
+    print("the number is an odd number ")
